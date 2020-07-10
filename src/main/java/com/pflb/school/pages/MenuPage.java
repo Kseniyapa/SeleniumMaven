15c01ec9c197996905ab14fe46fdd0d1c6792023
kseniyapanshina <kseniyapanshinana@gmail.com>
2020-07-10 09:03:17 +0300

    update

--- src/main/java/com/pflb/school/pages/MenuPage.java
+++ src/main/java/com/pflb/school/pages/MenuPage.java
@@ -5,8 +5,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class MenuPage {
 
